d14c6ebc1b10251ac9121558e9815cd44b4acd1c
syj0803 <sdudwlss@naver.com>
2017-07-24 17:23:41 +0900

    Hello.java modified on local repository.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
+//For command line git tutorial
+////Local Repository modification
+
 public class Hello{
 	public static void main(String[] args){
     		System.out.println("Hello Git");
+		System.out.println("what is Git?");
+		System.out.println("That's DVCS");
+	
 	}
 }
 
