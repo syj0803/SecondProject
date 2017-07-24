e9f7655b7679e803b879d1bf1d7d9f33f010e0a2
syj0803 <sdudwlss@naver.com>
2017-07-24 17:19:09 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@
 public class Hello{
 	public static void main(String[] args){
     		System.out.println("Hello Git");
+		System.out.println("What is Git?");
+		System.out.println("DVCS");
+		System.out.println("LINUX");
 	}
 }
 
